2b58902887f77ee68d3ff39dc24c1775663f01ed
Yaroslav <proskurin.yarik@gmail.com>
2025-05-05 13:54:12 +0200

    feat: implement NumInList algorithm

--- module01/num_in_list.go
+++ module01/num_in_list.go
@@ -3,5 +3,14 @@ package module01
 // NumInList will return true if num is in the
 // list slice, and false otherwise.
 func NumInList(list []int, num int) bool {
+	if len(list) == 0 {
+		return false
+	}
+
+	for _, item := range list {
+		if item == num {
+			return true
+		}
+	}
 	return false
 }
