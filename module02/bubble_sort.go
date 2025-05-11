4ef7bf0e8d6cb92056c3fde3426b00a83eac4be1
Yaroslav <proskurin.yarik@gmail.com>
2025-05-11 13:19:51 +0200

    feat: add BubbleSortInt using for loop and swap flag

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -5,7 +5,19 @@ import "sort"
 // BubbleSortInt will sort a list of integers using the bubble sort algorithm.
 //
 // Big O: O(N^2), where N is the size of the list
+// ![A list containing the numbers 5, 4, 2, 3, 1, 0](/img/bubble_sort_01.png)
+
 func BubbleSortInt(list []int) {
+	swapped := true
+	for swapped {
+		swapped = false
+		for i, j := 0, 1; j < len(list); i, j = i+1, j+1 {
+			if list[i] > list[j] {
+				swapped = true
+				list[i], list[j] = list[j], list[i]
+			}
+		}
+	}
 }
 
 // BubbleSortString is a bubble sort for string slices. Try implementing it for
