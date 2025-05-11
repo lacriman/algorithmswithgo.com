3346f8647f5356cbff4988fe328b26f7193df58d
Yaroslav <proskurin.yarik@gmail.com>
2025-05-11 15:09:59 +0200

    feat: add BubbleSortPerson to sort  Person objects by age, LastName, and FirstName

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -1,6 +1,9 @@
 package module02
 
-import "sort"
+import (
+	"sort"
+	"strings"
+)
 
 // BubbleSortInt will sort a list of integers using the bubble sort algorithm.
 //
@@ -23,11 +26,33 @@ func BubbleSortInt(list []int) {
 // BubbleSortString is a bubble sort for string slices. Try implementing it for
 // practice.
 func BubbleSortString(list []string) {
+	swapped := true
+	for swapped {
+		swapped = false
+		for i, j := 0, 1; j < len(list); i, j = i+1, j+1 {
+			if strings.ToLower(list[i]) > strings.ToLower(list[j]) {
+				swapped = true
+				list[i], list[j] = list[j], list[i]
+			}
+		}
+	}
 }
 
 // BubbleSortPerson uses bubble sort to sort Person slices by: Age, then
 // LastName, then FirstName. Try implementing it for practice.
 func BubbleSortPerson(people []Person) {
+	swapped := true
+	for swapped {
+		swapped = false
+		for i, j := 0, 1; j < len(people); i, j = i+1, j+1 {
+			if people[i].Age > people[j].Age ||
+				(people[i].Age == people[j].Age && people[i].LastName > people[j].LastName) ||
+				(people[i].Age == people[j].Age && people[i].LastName == people[j].LastName && people[i].FirstName > people[j].FirstName) {
+				people[i], people[j] = people[j], people[i]
+				swapped = true
+			}
+		}
+	}
 }
 
 // BubbleSort uses the standard library's sort.Interface to sort. Try
