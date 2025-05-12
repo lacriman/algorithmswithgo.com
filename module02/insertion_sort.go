11d85760665f08be191b0fd099f0a75b4e56617f
Yaroslav <proskurin.yarik@gmail.com>
2025-05-12 09:14:41 +0200

    feat: add InsertionSortInt for integer slices

--- module02/insertion_sort.go
+++ module02/insertion_sort.go
@@ -10,7 +10,19 @@ import "sort"
 //
 // Test with: go test -run InsertionSortInt$
 // The '$' at the end will ensure that the InsertionSortInterface tests won't be run.
+
+// list := []int{99, 3, 1, 5}
+
 func InsertionSortInt(list []int) {
+	for i := 1; i < len(list); i++ {
+		key := list[i]
+		j := i - 1
+		for j >= 0 && list[j] > key {
+			list[j+1] = list[j]
+			j--
+		}
+		list[j+1] = key
+	}
 }
 
 // InsertionSortString uses insertion sort to sort string slices. Try
